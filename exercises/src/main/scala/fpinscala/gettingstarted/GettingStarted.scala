747e11e295da47b44c49b8a54f177c7cd484dcc9
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2015-12-04 16:38:44 -0500

    gettingstarted: define fib(Int): Int

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -36,7 +36,14 @@ object MyModule {
 
   // Exercise 1: Write a function to compute the nth fibonacci number
 
-  def fib(n: Int): Int = ???
+  def fib(n: Int): Int = {
+    @annotation.tailrec
+    def go(i: Int, a: Int, b: Int): Int =
+        if (i == 0) a
+        else go(i - 1, b, a + b)
+
+    go(n, 0, 1)
+  }
 
   // This definition and `formatAbs` are very similar..
   private def formatFactorial(n: Int) = {
